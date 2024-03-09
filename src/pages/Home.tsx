3e6759447172422ab2f0822f3e74fd58b80684d9
Auxior <skossm@outlook.com>
2024-03-09 14:47:22 +0800

    Add navigation and links to Home page, implement login page, and add dynamic parameter to Edit page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
 import React, { FC } from 'react'
+import { useNavigate, Link } from 'react-router-dom'
 
 const Home: FC = () => {
-  return <p>Home</p>
+  const nav = useNavigate()
+
+  function clickHandler() {
+    nav('/login')
+  }
+
+  return (
+    <div>
+      <p>Home</p>
+      <div>
+        <button onClick={clickHandler}>登录</button>
+        <Link to="/register">注册</Link>
+      </div>
+    </div>
+  )
 }
 
 export default Home
