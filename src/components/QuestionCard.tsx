1378c1a9b7f356e4975f8f707d7be59b5911167d
Auxior <skossm@outlook.com>
2024-03-09 12:58:44 +0800

    Add sass package and update List page

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,48 +1,21 @@
-import React, { FC, useState } from 'react'
+import React, { FC } from 'react'
 import styles from './QuestionCard.module.scss'
 
-const rawQuestionList = [
-  {
-    id: 'q1',
-    title: '问卷1',
-    isPublished: false,
-    isStar: false,
-    answerCount: 5,
-    createAt: '3月10日 13:23',
-  },
-  {
-    id: 'q2',
-    title: '问卷2',
-    isPublished: true,
-    isStar: true,
-    answerCount: 3,
-    createAt: '3月11日 13:23',
-  },
-  {
-    id: 'q3',
-    title: '问卷3',
-    isPublished: false,
-    isStar: false,
-    answerCount: 6,
-    createAt: '3月12日 13:23',
-  },
-  ,
-  {
-    id: 'q4',
-    title: '问卷4',
-    isPublished: true,
-    isStar: false,
-    answerCount: 2,
-    createAt: '3月9日 13:23',
-  },
-]
+type PropsType = {
+  _id: string
+  title: string
+  isStar: boolean
+  isPublished: boolean
+  answerCount: number
+  createdAt: string
+}
 
-const QuestionCard: FC = () => {
-  const [questionList, setQuestionList] = useState(rawQuestionList)
-
+const QuestionCard: FC<PropsType> = (props: PropsType) => {
+  const { _id } = props
+  
   return (
     <>
-      <p>QuestionCard</p>
+      <p>QuestionCard {_id}</p>
     </>
   )
 }
