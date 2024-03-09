862b243af1bdbb4d41d906b0e82886678daad0d1
Auxior <skossm@outlook.com>
2024-03-09 13:17:19 +0800

    Add styles to QuestionCard component

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -11,12 +11,34 @@ type PropsType = {
 }
 
 const QuestionCard: FC<PropsType> = (props: PropsType) => {
-  const { _id } = props
+  const { _id, title, createdAt, answerCount, isPublished } = props
   
   return (
-    <>
-      <p>QuestionCard {_id}</p>
-    </>
+    <div className={styles.container}>
+      <div className={styles.title}>
+        <div className={styles.left}>
+          <a href="#">{title}</a>
+        </div>
+        <div className={styles.right}>
+          {isPublished ? <span style={{ color: 'green' }}>已发布</span> : <span>未发布</span>}
+          &nbsp;
+          <span>答卷: {answerCount}</span>
+          &nbsp;
+          <span>{createdAt}</span>
+        </div>
+      </div>
+      <div className={styles['button-container']}>
+        <div className={styles.left}>
+          <button>编辑问卷</button>
+          <button>数据统计</button>
+        </div>
+        <div className={styles.right}>
+          <button>标星</button>
+          <button>复制</button>
+          <button>删除</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
