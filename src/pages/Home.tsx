99bdffeebe810c074902023c4aef038420ee2945
Auxior <skossm@outlook.com>
2024-03-10 22:43:44 +0800

    Update MainLayout and add Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,21 +1,29 @@
 import React, { FC } from 'react'
-import { useNavigate, Link } from 'react-router-dom'
-import { Button } from 'antd'
+import { useNavigate } from 'react-router-dom'
+import { Button, Typography } from 'antd'
+import { MANAGE_INDEX__PATHNAME } from '../router'
+import styles from './Home.module.scss'
+
+const { Title, Paragraph } = Typography
 
 const Home: FC = () => {
   const nav = useNavigate()
 
-  function clickHandler() {
-    nav('/login')
-  }
-
   return (
-    <div>
-      <p>Home</p>
-      <div>
-        <Button onClick={clickHandler}>登录</Button>
-        &nbsp;
-        <Link to="/register">注册</Link>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <Title>问卷调查 | 在线投票</Title>
+        <Paragraph>已累计创建问卷 100 份，发布问卷 90 份，收到答卷 980 份</Paragraph>
+        <div>
+          <Button
+            type="primary"
+            onClick={() => {
+              nav(MANAGE_INDEX__PATHNAME)
+            }}
+          >
+            开始使用
+          </Button>
+        </div>
       </div>
     </div>
   )
