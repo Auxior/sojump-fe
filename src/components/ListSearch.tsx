bdec4ce990e77c3ad2c88b46a7ec35fb2ed10e32
Auxior <skossm@outlook.com>
2024-03-12 22:03:12 +0800

    Add ListSearch component to Star and Trash pages

--- src/components/ListSearch.tsx
+++ src/components/ListSearch.tsx
@@ -1,16 +1,30 @@
-import React, { ChangeEvent, FC, useState } from 'react'
+import React, { FC, useState, useEffect } from 'react'
+import type { ChangeEvent } from 'react'
+import { useNavigate, useLocation, useSearchParams } from 'react-router-dom'
 import { Input } from 'antd'
+import { LIST_SEARCH_PARAM_KEY } from '../constant'
 
 const { Search } = Input
 
 const ListSearch: FC = () => {
+  const nav = useNavigate()
+  const { pathname } = useLocation()
+
   const [value, setValue] = useState('')
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     setValue(event.target.value)
   }
 
+  // 获取 url 参数，并设置到 input value
+  const [searchParams] = useSearchParams()
+  useEffect(() => {
+    const curValue = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+    setValue(curValue)
+  }, [searchParams])
+
   function handleSearch(value: string) {
-    console.log('[ value ] >', value)
+    // 跳转页面，增加 url 参数
+    nav({ pathname, search: `${LIST_SEARCH_PARAM_KEY}=${value}` })
   }
 
   return <Search size="large" allowClear placeholder="输入关键字" value={value} onChange={handleChange} onSearch={handleSearch} style={{ width: '260px' }} />
