da60b4e043ba6a8809456b4b8c601a64e17680c7
Auxior <skossm@outlook.com>
2024-03-17 17:13:45 +0800

    Refactor List component to implement infinite scrolling

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,9 +1,11 @@
-import React, { FC } from 'react'
-import { Typography, Spin } from 'antd'
-import { useTitle } from 'ahooks'
+import React, { FC, useEffect, useState, useRef, useMemo } from 'react'
+import { Typography, Spin, Empty } from 'antd'
+import { useTitle, useDebounceFn, useRequest } from 'ahooks'
+import { useSearchParams } from 'react-router-dom'
+import { getQuestionListService } from '../../services/question'
 import QuestionCard from '../../components/QuestionCard'
 import ListSearch from '../../components/ListSearch'
-import useLoadQuestionListData from '../../hooks/useLoadQuestionListData'
+import { LIST_PAGE_SIZE, LIST_SEARCH_PARAM_KEY } from '../../constant'
 import styles from './common.module.scss'
 
 const { Title } = Typography
@@ -11,8 +13,84 @@ const { Title } = Typography
 const List: FC = () => {
   useTitle('问卷星 - 我的问卷')
 
-  const { data = {}, loading } = useLoadQuestionListData()
-  const { list = [], total = 0 } = data
+  const [started, setStarted] = useState(false) // 是否已经开始加载（防抖，有延迟时间）
+  const [page, setPage] = useState(1)
+  const [list, setList] = useState([]) // 全部的列表数据，上划加载更多，累计
+  const [total, setTotal] = useState(0)
+  const haveMoreData = total > list.length // 有没有更多的、未加载完成的数据
+
+  const [searchParams] = useSearchParams()
+  const keyword = searchParams.get(LIST_SEARCH_PARAM_KEY) || ''
+
+  // keyword 变化时，重置信息
+  useEffect(() => {
+    setStarted(false)
+    setPage(1)
+    setList([])
+    setTotal(0)
+  }, [keyword])
+
+  // 真正加载
+  const { run: load, loading } = useRequest(
+    async () => {
+      const data = await getQuestionListService({
+        page,
+        pageSize: LIST_PAGE_SIZE,
+        keyword,
+      })
+      return data
+    },
+    {
+      manual: true,
+      onSuccess(result) {
+        const { list: l = [], total = 0 } = result
+        setList(list.concat(l)) // 累计
+        setTotal(total)
+        setPage(page + 1)
+      },
+    }
+  )
+
+  // 尝试触发加载 - 防抖
+  const containerRef = useRef<HTMLDivElement>(null)
+  const { run: tryLoadMore } = useDebounceFn(
+    () => {
+      const elem = containerRef.current
+      if (elem == null) return
+      const domRect = elem.getBoundingClientRect()
+      if (domRect == null) return
+      const { bottom } = domRect
+      if (bottom <= window.innerHeight) {
+        load() // 真正加载数据
+        setStarted(true)
+      }
+    },
+    { wait: 1000 }
+  )
+
+  // 1. 当页面加载，或者 url 参数（keyword）变化时，触发加载
+  useEffect(() => {
+    tryLoadMore() // 加载第一页，初始化
+  }, [searchParams])
+
+  // 2. 当页面滚动时，尝试触发加载
+  useEffect(() => {
+    if (haveMoreData) {
+      window.addEventListener('scroll', tryLoadMore) // 防抖
+    }
+
+    return () => {
+      window.removeEventListener('scroll', tryLoadMore) // 解绑事件
+    }
+  }, [searchParams, haveMoreData])
+
+  // LoadMore Elem
+  const LoadMoreContentElem = useMemo(() => {
+    if (!started || loading) return <Spin />
+    if (total === 0) return <Empty description="暂无数据" />
+    if (!haveMoreData) return <span>没有更多了...</span>
+    return <span>开始加载下一页</span>
+  }, [started, loading, haveMoreData])
 
   return (
     <>
@@ -25,21 +103,16 @@ const List: FC = () => {
         </div>
       </div>
       <div className={styles.container}>
-        {loading && (
-          <div style={{ textAlign: 'center' }}>
-            <Spin />
-          </div>
-        )}
         {/* 问卷列表 */}
-        {!loading &&
-          list.length > 0 &&
+        {list.length > 0 &&
           list.map((q: any) => {
             const { _id } = q
-
             return <QuestionCard key={_id} {...q} />
           })}
       </div>
-      <div className={styles.footer}>loadMore... 上划加载更多...</div>
+      <div className={styles.footer}>
+        <div ref={containerRef}>{LoadMoreContentElem}</div>
+      </div>
     </>
   )
 }
