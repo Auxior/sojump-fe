6be089ce5f371b4d133050b2a4109563834987b7
Auxior <skossm@outlook.com>
2024-03-23 16:23:08 +0800

    Add useLoadUserData hook to MainLayout and QuestionLayout

--- src/layouts/QuestionLayout.tsx
+++ src/layouts/QuestionLayout.tsx
@@ -1,12 +1,22 @@
 import React, { FC } from 'react'
+import { Spin } from 'antd'
 import { Outlet } from 'react-router-dom'
+import useLoadUserData from '../hooks/useLoadUserData'
 
 const QuestionLayout: FC = () => {
+  const { waitingUserData } = useLoadUserData()
+
   return (
     <>
       <p>Question layout</p>
       <div>
-        <Outlet />
+        {waitingUserData ? (
+          <div style={{ textAlign: 'center', marginTop: '60px' }}>
+            <Spin />
+          </div>
+        ) : (
+          <Outlet />
+        )}
       </div>
     </>
   )
