ad56e12d7456d4032424f26af78f506008e2d8f5
Auxior <skossm@outlook.com>
2024-03-24 14:05:20 +0800

    Add component generation to ComponentLib

--- src/pages/question/Edit/ComponentLib.tsx
+++ src/pages/question/Edit/ComponentLib.tsx
@@ -1,20 +1,34 @@
 import React, { FC } from 'react'
 import { Typography } from 'antd'
-import { componentConfGroup } from '../../../components/QuestionComponents'
+import { componentConfGroup, ComponentConfType } from '../../../components/QuestionComponents'
+import styles from './ComponentLib.module.scss'
 
 const { Title } = Typography
+
+function genComponent(c: ComponentConfType) {
+  const { title, type, Component } = c
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.component}>
+        <Component />
+      </div>
+    </div>
+  )
+}
 
 const Lib: FC = () => {
   return (
     <>
       {componentConfGroup.map((group, index) => {
-        const { groupId, groupName } = group
+        const { groupId, groupName, components } = group
 
         return (
           <div key={groupId}>
             <Title level={3} style={{ fontSize: '16px', marginTop: index > 0 ? '20px' : 0 }}>
               {groupName}
             </Title>
+            <div>{components.map((c) => genComponent(c))}</div>
           </div>
         )
       })}
