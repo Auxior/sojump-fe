328c0e3759809e5137f8f8167273b84097e0c6d9
Auxior <skossm@outlook.com>
2024-03-24 13:56:06 +0800

    Add componentConfGroup and LeftPanel component

--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux'
 import { changeSelectedId } from '../../../store/componentsReducer'
 import useLoadQuestionData from '../../../hooks/useLoadQuestionData'
 import EditCanvas from './EditCanvas'
+import LeftPanel from './LeftPanel'
 import styles from './index.module.scss'
 
 const Edit: FC = () => {
@@ -19,7 +20,9 @@ const Edit: FC = () => {
       <div style={{ backgroundColor: '#fff', height: '40px' }}>Header</div>
       <div className={styles['content-wrapper']}>
         <div className={styles.content}>
-          <div className={styles.left}>Left</div>
+          <div className={styles.left}>
+            <LeftPanel />
+          </div>
           <div className={styles.main} onClick={clearSelectedId}>
             <div className={styles['canvas-wrapper']}>
               <EditCanvas loading={loading} />
