b9d4db482e39155b3071e94c181409061f3303c3
Auxior <skossm@outlook.com>
2024-03-24 14:31:13 +0800

    Add nanoid package and implement addComponent function

--- src/pages/question/Edit/ComponentLib.tsx
+++ src/pages/question/Edit/ComponentLib.tsx
@@ -1,15 +1,31 @@
 import React, { FC } from 'react'
+import { nanoid } from 'nanoid'
 import { Typography } from 'antd'
+import { useDispatch } from 'react-redux'
 import { componentConfGroup, ComponentConfType } from '../../../components/QuestionComponents'
+import { addComponent } from '../../../store/componentsReducer'
 import styles from './ComponentLib.module.scss'
 
 const { Title } = Typography
 
 function genComponent(c: ComponentConfType) {
-  const { title, type, Component } = c
+  const { title, type, Component, defaultProps } = c
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const dispatch = useDispatch()
+
+  function handleClick() {
+    dispatch(
+      addComponent({
+        fe_id: nanoid(),
+        type,
+        title,
+        props: defaultProps,
+      })
+    )
+  }
 
   return (
-    <div className={styles.wrapper}>
+    <div key={type} className={styles.wrapper} onClick={handleClick}>
       <div className={styles.component}>
         <Component />
       </div>
--- src/store/componentsReducer/index.ts
+++ src/store/componentsReducer/index.ts
@@ -31,9 +31,26 @@ export const componentsSlice = createSlice({
     changeSelectedId: produce((draft: ComponentsStateType, action: PayloadAction<string>) => {
       draft.selectedId = action.payload
     }),
+    // 添加新组件
+    addComponent: produce((draft: ComponentsStateType, action: PayloadAction<ComponentInfoType>) => {
+      const newComponent = action.payload
+
+      const { selectedId, componentList } = draft
+      const index = componentList.findIndex((c) => c.fe_id === selectedId)
+
+      if (index < 0) {
+        // 未选中任何组件
+        draft.componentList.push(newComponent)
+      } else {
+        // 选中了组件，插入到 index 后面
+        draft.componentList.splice(index + 1, 0, newComponent)
+      }
+
+      draft.selectedId = newComponent.fe_id
+    }),
   },
 })
 
-export const { resetComponents, changeSelectedId } = componentsSlice.actions
+export const { resetComponents, changeSelectedId, addComponent } = componentsSlice.actions
 
 export default componentsSlice.reducer
